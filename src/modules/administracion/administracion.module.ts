4ab97eb67ca86792b5c10721ae628a02bc2d2e90
Rey9530 <alex.rey.rosales.95@gmail.com>
2025-11-01 00:28:39 -0600

    CRUD de clientes

--- src/modules/administracion/administracion.module.ts
+++ src/modules/administracion/administracion.module.ts
@@ -9,10 +9,36 @@ import { UsuariosController } from './usuarios/usuarios.controller';
 import { UsuariosService } from './usuarios/usuarios.service';
 import { RolesService } from './roles/roles.service';
 import { RolesController } from './roles/roles.controller';
-@Module({
+import { DepartamentosController } from './departamentos/departamentos.controller';
+import { DepartamentosService } from './departamentos/departamentos.service';
+import { MunicipiosController } from './municipios/municipios.controller';
+import { MunicipiosService } from './municipios/municipios.service';
+import { ColoniasController } from './colonias/colonias.controller';
+import { ColoniasService } from './colonias/colonias.service';
+import { DteCatalogosController } from './dte-catalogos/dte-catalogos.controller';
+import { DteCatalogosService } from './dte-catalogos/dte-catalogos.service';
 
+@Module({
   imports: [AuthModule, PrismaModule],
-  controllers: [CategoriasController, CatalogoController, UsuariosController, RolesController],
-  providers: [CategoriasService, CatalogoService, UsuariosService, RolesService],
+  controllers: [
+    CategoriasController,
+    CatalogoController,
+    UsuariosController,
+    RolesController,
+    DepartamentosController,
+    MunicipiosController,
+    ColoniasController,
+    DteCatalogosController,
+  ],
+  providers: [
+    CategoriasService,
+    CatalogoService,
+    UsuariosService,
+    RolesService,
+    DepartamentosService,
+    MunicipiosService,
+    ColoniasService,
+    DteCatalogosService,
+  ],
 })
 export class AdministracionModule { }
